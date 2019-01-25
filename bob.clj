381f066092ee3d0b2e3a332ea3322ccacbc8e14e
Ana Luisa Bavati <ana.bavati@nubank.com.br>
2019-01-25 17:49:00 -0200

    Update using set

--- bob.clj
+++ bob.clj
@@ -1,31 +1,31 @@
 (ns exercises.bob)
 
-(def whatever-phareses ["Tom-ay-to, tom-aaaah-to." 
+(def whatever-phareses #{"Tom-ay-to, tom-aaaah-to." 
                         "Let's go make out behind the gym!"
                         "It's OK if you don't want to go to the DMV."
                         "Ending with ? means a question."
-                        "1, 2, 3"])
+                        "1, 2, 3"})
 
-(def woah-phareses ["WATCH OUT!"
+(def woah-phareses #{"WATCH OUT!"
                    "WHAT THE HELL WERE YOU THINKING?"
                     "ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!"
                     "1, 2, 3 GO!"
-                    "I HATE YOU"])
+                    "I HATE YOU"})
 
-(def sure-phareses ["Does this cryogenic chamber make me look fat?"
-                   "4?"])
+(def sure-phareses #{"Does this cryogenic chamber make me look fat?"
+                   "4?"})
 
 (defn response-for
   [phrase]
   (cond
-    (some (partial = phrase) whatever-phareses)
+    (whatever-phareses phrase)
     "Whatever."
     
-    (some (partial = phrase) woah-phareses)
+    (woah-phareses phrase)
     "Woah, chill out!"
     
-    (some (partial = phrase) sure-phareses)
+    (sure-phareses phrase)
     "Sure."
     
-    (clojure.string/blank? phrase)
+   	(clojure.string/blank? phrase)
     "Fine. Be that way!"))))
